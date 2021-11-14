77e868fd1582032bef2d391d1bd45298d7c42c42
Alexandra Lem <a_lem@fanshaweonline.ca>
2021-11-14 08:31:13 +0600

    adding more changes

--- gameComponents/comparison.py
+++ gameComponents/comparison.py
@@ -4,34 +4,34 @@ from colorama import Fore, Back, Style
 init(autoreset=True)
 
 def comparisonfunction(computer):
-        if (computer == gameVars.player):
+        if computer == gameVars.player:
                 print(Fore.BLUE + "tie! try again")
 
-        elif (gameVars.player == "rock"):
-            if (computer == "paper"):
-                print(Fore.RED + "you lose!")
+        elif gameVars.player == "rock":
+            if computer == "paper":
+                print(Fore.RED + "******you lose!******")
                 gameVars.playerLives = gameVars.playerLives -1
-                livesCount.livesdisplay(gameVars.playerLives)
 
             else:
-                print(Fore.GREEN + "you win!")
-
+                print(Fore.GREEN + "******you win!******")
                 gameVars.computerLives = gameVars.computerLives -1
 
 
-        elif (gameVars.player == "paper"):
-            if (computer == "scissors"):
-                print("you lose!")
+        elif gameVars.player == "paper":
+            if computer == "scissors":
+                print(Fore.RED + "******you lose!******")
                 gameVars.playerLives = gameVars.playerLives -1
+                
             else:
-                print("you win!")
+                print(Fore.GREEN + "******you win!******")
                 gameVars.computerLives = gameVars.computerLives -1
 
 
-        elif (gameVars.player == "scissors"):    
-            if (computer == "rock"):
-                print("you lose!")
+        elif gameVars.player == "scissors":    
+            if computer == "rock":
+                print(Fore.RED + "******you lose!******")
                 gameVars.playerLives = gameVars.playerLives -1
+                
             else:
-                print("you win!")
+                print(Fore.GREEN + "******you win!******")
                 gameVars.computerLives = gameVars.computerLives -1
--- main.py
+++ main.py
@@ -1,34 +1,28 @@
 from random import randint
-from gameComponents import winLose, gameVars
+from gameComponents import winLose, gameVars, livesCount
 from gameComponents import comparison
 
 
-# an array is just a container. It holds multiple values in a 0-based index
-# you can store anything in an array and retrieve it later. Arrays have square brackets notation
-
-
-# define a win/lose function and invoke it in our game loop when lives run out (player or computer)
-
-# create an infinite loop (for now) so that we can keep playing
 while gameVars.player is False:
         gameVars.player = input("Choose rock, paper or scissors: ")
         computer = gameVars.choices[randint (0,2)]
 
         comparison.comparisonfunction(computer)
 
-        print ("player chose:" + gameVars.player)
-        print ("computer chose: " + computer)
+        print ("======player chose:" + gameVars.player + "======")
+        print ("======computer chose: " + computer + "======")
 
         print("computer lives: " + str(gameVars.computerLives))
+        livesCount.computerlivesdisplay(gameVars.computerLives)
+
         print("player lives: " + str(gameVars.playerLives))
+        livesCount.playerlivesdisplay(gameVars.playerLives)
 
         if gameVars.playerLives == 0:
-            # call the winorlose function here
             winLose.winorlose("lost")
 
 
         elif gameVars.computerLives == 0:
-            # call the winorlose function here
             winLose.winorlose("won")
 
         gameVars.player = False
